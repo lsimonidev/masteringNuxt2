54e4ef9d07ab46f539b4d7f85634ac2ac82b4c86
Laura Simoni <laura@Lauras-MacBook-Pro.local>
2023-02-24 16:05:52 +0000

    customizing error page

---
+++ layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <img src="https://i.giphy.com/media/aYpmlCXgX9dc09dbpl/giphy.webp" alt="" />
+    <br />
+    {{ error.message }} {{ error.statusCode }}
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: "OOPS!!!",
+    };
+  },
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+<style scoped>
+.container {
+  text-align: center;
+}
+</style>
